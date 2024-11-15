30d9e678c8dcaccf12d82cceba7e37e6265185a4
César Ibarra Trejo <ces2294@gmail.com>
2024-11-14 20:39:19 -0600

    Completed Buddy Prefab
    Completed the code for Buddy Prefab and imported TextMesh textures.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor.Animations;
 using UnityEngine;
 
@@ -12,16 +13,30 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float minDoubleJumpForce = 1f;
     [SerializeField] private float maxDoubleJumpForce = 4f;
     [SerializeField] private float jumpLag = 0.2f;
+    [SerializeField] private float runStartThreshold = 0.5f;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private AudioClip jumpSound;
+    [SerializeField] private AudioClip attackSound;
+    [SerializeField] private AudioClip collectSound;
+    [SerializeField] private AudioClip stompSound;
     private Animator animator;
     private Rigidbody2D rigidBody;
     private Collider2D groundChecker;
+    private AudioSource audioSource;
     private bool isWalking;
     private bool isJumping;
     private bool isRunning;
+    private bool canRun;
     private bool canJump;
     private bool canDoubleJump;
     private float jumpCount;
     private float jumpForce;
+    private float runCount;
+    private float lastDirection;
+    private bool isLanded;
+    private bool isFalling;
+    private float airborneHorizVelocity;
+    private bool canMove;
 
     public const string IS_LANDED_NAME = "isLanded";
 
@@ -30,44 +45,95 @@ public class PlayerController : MonoBehaviour
         groundChecker = GetComponentInChildren<BoxCollider2D>();
         animator = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody2D>();
-        animator.SetBool(IS_LANDED_NAME, true);
+        audioSource = GetComponent<AudioSource>();  
         isWalking = false;
         isRunning = false;
         isJumping = false;
         canJump = true;
         canDoubleJump = true;
+        isLanded = true;
         jumpCount = -1;
+        runCount = -1;
         jumpForce = minJumpForce;
+        airborneHorizVelocity = 0;
+        canMove = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(jumpCount);
-        animator.SetBool("isFalling", false);
-        animator.SetBool("isWalking", false);
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        if (horizontal != 0f)
-        {
-            walk(horizontal);
-
-        }
-        jump();
-        if (rigidBody.velocity.y < -0.0001)
-        {
-            animator.SetBool("isFalling", true);
-        } else
+        if (canMove)
         {
             animator.SetBool("isFalling", false);
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", false);
+            animator.SetBool(IS_LANDED_NAME, false);
+            float vertical = Input.GetAxisRaw("Vertical");
+            rigidBody.velocity += Vector2.right * airborneHorizVelocity;
+            walk();
+            jump();
+            if (rigidBody.velocity.y < -0.0001)
+            {
+                isFalling = true;
+            }
+            else if (rigidBody.velocity.y > -0.0001)
+            {
+                isFalling = false;
+            }
+            if (isLanded)
+            {
+                audioSource.pitch = 1;
+            }
+            animator.SetBool("isLanded", isLanded);
+            animator.SetBool("isFalling", isFalling);
+            animator.SetBool("isWalking", isWalking);
+            animator.SetBool("isRunning", isRunning);
         }
+        text.text = $"isFalling: {isFalling}\nisLanded: {isLanded}\nCan Jump: {canJump}" +
+            $"\nCan Double Jump: {canDoubleJump}\nisWalking: {isWalking}\nisRunning: {isRunning}\nCan run: {canRun}\nrunCount {runCount}\nAirborne Velocity X: {airborneHorizVelocity}" +
+            $"\nVelocity X: {rigidBody.velocity.x}";
+
     }
 
-    private void walk(float horizontal)
+    private void walk()
     {
-        transform.Translate(horizontal * walkSpeed * Time.deltaTime, 0, 0);
-        transform.localScale = new Vector3(horizontal, 1);
-        animator.SetBool("isWalking", true);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        if (runCount > -1 && runCount < runStartThreshold)
+        {
+            runCount += Time.deltaTime;
+        }
+        if (horizontal != 0f)
+        {
+            if (isLanded && !isRunning && runCount < 0)
+            {
+                runCount = 0;
+            }
+            isWalking = true;
+            if (isLanded && !isRunning && canRun)
+            {
+                isRunning = true;
+                canRun = false;
+                runCount = -1;
+            };
+            transform.Translate(horizontal * (isRunning ? runSpeed : walkSpeed) * Time.deltaTime, 0, 0);
+            transform.localScale = new Vector3(horizontal, 1);
+        }
+        else
+        {
+            if (isRunning)
+            {
+                runCount = 0;
+                canRun = true;
+            }
+            canRun = runCount != -1 && runCount < runStartThreshold;
+            if (!canRun)
+            {
+                runCount = -1;
+            }
+            isRunning = false;
+            isWalking = false;
+        }
+        
     }
 
     private void jump()
@@ -83,7 +149,8 @@ public class PlayerController : MonoBehaviour
             {
                 canDoubleJump = false;
                 isDoubleJump = true;
-                animator.SetTrigger("doubleJump");
+                audioSource.pitch = 0.7f;
+                jumpCount = jumpLag + 1;
             }
             else
             {
@@ -103,10 +170,10 @@ public class PlayerController : MonoBehaviour
         }
         if (jumpCount > jumpLag)
         {
-            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            
+            airborneHorizVelocity = rigidBody.velocity.x;
+            audioSource.PlayOneShot(jumpSound);
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
             jumpCount = -1;
-            animator.SetBool("isLanded", false);
         }
     }
 
@@ -115,9 +182,9 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Tilemap"))
         {
-            animator.SetBool(IS_LANDED_NAME, true);
             canJump = true;
             canDoubleJump = true;
+            isLanded = true;
         }   
     }
 
@@ -125,7 +192,8 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Tilemap"))
         {
-            animator.SetBool(IS_LANDED_NAME, false);
+            canJump = false;
+            isLanded = false;
         }
     }
 }
